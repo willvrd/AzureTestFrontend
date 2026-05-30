6cb8700f810c549453551e7113d2283eb4f0f86a
William <williamverde10@gmail.com>
2026-05-30 19:27:02 -0400

    Fix: staging merge

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,37 +4,6 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig, fontProviders } from 'astro/config';
 
-<<<<<<< HEAD
-// https://astro.build/config
-export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
-	fonts: [
-		{
-			provider: fontProviders.local(),
-			name: 'Atkinson',
-			cssVariable: '--font-atkinson',
-			fallbacks: ['sans-serif'],
-			options: {
-				variants: [
-					{
-						src: ['./src/assets/fonts/atkinson-regular.woff'],
-						weight: 400,
-						style: 'normal',
-						display: 'swap',
-					},
-					{
-						src: ['./src/assets/fonts/atkinson-bold.woff'],
-						weight: 700,
-						style: 'normal',
-						display: 'swap',
-					},
-				],
-			},
-		},
-	],
-});
-=======
 import node from '@astrojs/node';
 
 // Read the frontend URL from environment variables, fallback to example.com if not defined
@@ -84,4 +53,3 @@ export default defineConfig({
         mode: 'standalone', // Enables the backend server capabilities
     }),
 });
->>>>>>> staging
--- src/consts.ts
+++ src/consts.ts
@@ -1,10 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-<<<<<<< HEAD
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
-=======
 export const SITE_TITLE = 'AzureTest Frontend';
 export const SITE_DESCRIPTION = 'AzureTest Frontend';
->>>>>>> staging
